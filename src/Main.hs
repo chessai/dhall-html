4303480b2445b506c9c4dfdbacdfe792ffc81f40
goolord <zacharyachurchill@gmail.com>
2019-01-16 10:11:54 -0500

    init commit

---
+++ src/Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import Data.Text (Text)
+import Dhall
+import Text.Taggy.DOM
+import Text.Taggy.Renderer
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
+
+data Example = Example 
+  { name :: Dhall.Text
+  , upvotes :: Dhall.Natural 
+  }
+  deriving (Dhall.Generic, Show)
+
+instance Dhall.Interpret Example
+
+newline :: IO ()
+newline = putChar '\n'
+
+main :: IO ()
+main = do
+
+  newline
+
+  -- Print dhall file
+  people <- Dhall.input Dhall.auto "./misc/people"
+  mapM_ print (people :: [Example])
+
+  newline
+
+  ipsum <- T.readFile "./misc/ipsum.html"
+  mapM_ T.putStrLn $ render <$> parseDOM True ipsum
+
+  pure()
